1b52a7d63a7c861e794594fbe88ff7f354496d24
Lindsay Levine <lindslev.ll@gmail.com>
2021-05-13 15:24:14 -0400

    change api func

--- pages/api/repositories.js
+++ pages/api/repositories.js
@@ -1,5 +1,9 @@
-export default async (req, res) => {
-  const response = { hello: "world" };
-  res.json(response);
-  res.end();
-};
+// export default async (req, res) => {
+//   const response = { hello: "world" };
+//   res.json(response);
+//   res.end();
+// };
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}
