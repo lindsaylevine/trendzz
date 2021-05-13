94034d143276d27a59284139eec08bfb86689585
Lindsay Levine <lindslev.ll@gmail.com>
2021-05-13 03:57:48 -0400

    dont mind me just trimming

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,8 @@
 import React from 'react';
-import { NextPage } from 'next';
-import Head from 'next/head';
-import Header from '../components/Header';
-import BottomNav from '../components/BottomNav';
-import WithFetchFeedRepos from '../components/WithFetchFeedRepos';
-import Repos from '../components/Repos';
-import NoTrendingOctocat from '../components/NoTrendingOctocat';
 
-const Page: NextPage = () => (
-  <>
-    <Head>
-      <meta name="title" content="Trendzz | Feed" />
-      <title>Trendzz | Feed</title>
-    </Head>
-    <Header />
-    <WithFetchFeedRepos
-      hideOpened
-    >
-      {({
-        repos,
-        isFetchingRepos,
-      }): ReturnType<React.FC> => (
-        <>
-          <Repos
-            repos={repos}
-            isFetchingRepos={isFetchingRepos}
-            isBigHeight
-            EmptyPlaceholder={NoTrendingOctocat}
-            canMarkAsRead
-          />
-        </>
-      )}
-    </WithFetchFeedRepos>
-    <BottomNav />
-  </>
+const Page = () => (
+  <div>ok
+  </div>
 );
 
 export default Page;
