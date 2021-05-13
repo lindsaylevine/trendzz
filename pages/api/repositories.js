752c4c89b3a734b4f149efca6126ab7f7d55a3f0
Lindsay Levine <lindslev.ll@gmail.com>
2021-05-13 03:26:39 -0400

    try not ts

---
+++ pages/api/repositories.js
@@ -0,0 +1,5 @@
+export default async (req, res) => {
+  const response = { hello: "world" };
+  res.json(response);
+  res.end();
+};
--- pages/api/repositories.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  const response = { hello: "world" };
-  res.json(response);
-  res.end();
-};
