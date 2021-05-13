7e4cdf3e1c5c99d1647fa0339384f3476a880a05
Lindsay Levine <lindslev.ll@gmail.com>
2021-05-13 03:33:30 -0400

    lol nvm just src maps

--- next.config.js
+++ next.config.js
@@ -1,38 +1,35 @@
-module.exports = {
-  target: 'serverless',
-}
-// /* eslint-disable @typescript-eslint/no-var-requires */
-// const withPWA = require('next-pwa');
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-// const withCSS = require('@zeit/next-css');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const withCSS = require('@zeit/next-css');
 
-// // Use the hidden-source-map option when you don't want the source maps to be
-// // publicly available on the servers, only to the error reporting
+// Use the hidden-source-map option when you don't want the source maps to be
+// publicly available on the servers, only to the error reporting
 // const withSourceMaps = require('@zeit/next-source-maps')();
 
-// module.exports = withCSS(withPWA(withBundleAnalyzer(withSourceMaps({
-//   poweredByHeader: false,
-//   cssModules: true,
-//   target: 'serverless',
-//   cssLoaderOptions: {
-//     // typings-for-css-modules-loader config here
-//     namedExport: true,
-//   },
-//   tsCssModules: true,
-//   pwa: {
-//     dest: 'public',
-//     disable: process.env.NODE_ENV !== 'production',
-//   },
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       issuer: {
-//         test: /\.(js|ts)x?$/,
-//       },
-//       use: ['@svgr/webpack'],
-//     });
-//     return config;
-//   },
-// }))));
+module.exports = withCSS(withPWA(withBundleAnalyzer({
+  poweredByHeader: false,
+  cssModules: true,
+  target: 'serverless',
+  cssLoaderOptions: {
+    // typings-for-css-modules-loader config here
+    namedExport: true,
+  },
+  tsCssModules: true,
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV !== 'production',
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+})));
